c174e9f6dfbe3fc9311348fc031183472c998ded
Wrexan <fwrexan@gmail.com>
2022-01-30 17:07:08 +0200

    work with table + xls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import xlrd
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QSpacerItem, QSizePolicy
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton,\
+    QSpacerItem, QSizePolicy, QTableWidgetItem, QHeaderView
 from window_main import Ui_MainWindow
 
 # if __name__ == '__main__':
@@ -62,6 +63,10 @@ class App(QMainWindow):
         # text = self.input_search
 
         self.ui.input_search.textChanged[str].connect(self.search_and_upd_model_buttons)
+        self.ui.table_left.verticalHeader().setDefaultSectionSize(14)
+        self.ui.table_right.verticalHeader().setDefaultSectionSize(14)
+        self.ui.table_left.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.ui.table_left.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
         self.ui.table_left.setHorizontalHeaderLabels(['Виды работ', 'Цена', 'Примечание'])
         self.ui.table_right.setHorizontalHeaderLabels(['Тип', 'Фирма', 'Модель', 'Примечаiние',
                                                        'Цена', 'К-во', 'Дата', 'Где'])
@@ -69,8 +74,10 @@ class App(QMainWindow):
 
     def search_and_upd_model_buttons(self, search_req):
         self.search_price_models(search_req)
+        lay = self.ui.scroll_models_layout.layout()
+        self.clear_layout(lay)
         if self.models:
-            self.upd_model_buttons()
+            self.upd_model_buttons(lay)
 
     def search_price_models(self, search_req):
         search_req_len = len(search_req)
@@ -93,6 +100,7 @@ class App(QMainWindow):
                     if found_pos == -1: break
                     # if search request > 3 symbols, cut everything from left
                     # for smaller, cut from both sides (more strict)
+                    # print(f'{len(self.models)=} {self.models=} {name_cell=} {sheet=} {row_num=}')  # ERROR LOG
                     if (search_req_len > 2  # and (found_pos == 0 or name_cell[found_pos - 1] in "/\\ ")
                             or (search_req_len < 3 and (found_pos == 0 or name_cell[found_pos - 1] in "/\\ ")
                                 and (found_pos + search_req_len == b or
@@ -106,6 +114,7 @@ class App(QMainWindow):
                         #  search_req[-1] == ' '):
                         # print(f'FOUND============={name_cell=}')
                         le = len(self.models)
+                        # print(f'{le=} {self.models=} {name_cell=} {sheet=} {row_num=}')
                         if le < 5:
                             self.models[name_cell] = [sheet, row_num]
                             if le == 5:
@@ -118,9 +127,9 @@ class App(QMainWindow):
                     else:
                         a += found_pos + search_req_len
 
-    def upd_model_buttons(self):
-        lay = self.ui.scroll_models_layout.layout()
-        self.clear_layout(lay)
+    def upd_model_buttons(self, lay):
+        # lay = self.ui.scroll_models_layout.layout()
+        # self.clear_layout(lay)
         # lay.addWidget(QSpacerItem.sizePolicy)
 
         le = len(self.models)
@@ -140,23 +149,41 @@ class App(QMainWindow):
         position = self.models[b]
         # print(f'{position=}')
         # print(f'{position[0].name=}')
-        col = search_column_numbers[position[0].name] or search_column_numbers[0]
+        if position[0].name in search_column_numbers:
+            col = search_column_numbers[position[0].name]
+        else:
+            col = search_column_numbers['+']
         # print(f'{col=}')
         row = position[0].row_values(position[1], 0, 9)
         # if position[1] == position[0].nrows or position[0].row_values(position[1]+1, 0, 1):
         #     print(f'{row=} {position[1] == position[0].nrows=} {position[0].row_values(position[1]+1, 0, 1)=}')
         #     return
+        r = 0
+        # self.ui.table_left.clearContents()
+        self.ui.table_left.setRowCount(0)
         for i in range(position[1], position[0].nrows - 1):
             # print(row[col[0] - 1, col[1] - 1, col[2] - 1])
-            print(row[col[0] - 1], row[col[1] - 1], row[col[2] - 1])
-            if i < position[0].nrows:
-                # print(row[col[0] - 1, col[1] - 1, col[2] - 1])
-                row = position[0].row_values(i + 1, 0, 9)
-                if row[0] != '':
-                    return
-                # print(row[col[0]-1, col[1]-1, col[2]-1])
-            else:
+            if len(row) < (col[2]):
+                print(row)
                 return
+            else:
+                print(row) #===============================================================
+                r1 = str(row[col[1] - 1])
+                if row[col[0] - 1] or len(r1) > 3:
+                    self.ui.table_left.insertRow(r)
+                    self.ui.table_left.setItem(r, 0, QTableWidgetItem(str(row[col[0] - 1])))
+                    self.ui.table_left.setItem(r, 1, QTableWidgetItem(r1))
+                    self.ui.table_left.setItem(r, 2, QTableWidgetItem(str(row[col[2] - 1])))
+                    r += 1
+                    print(row[col[0] - 1], row[col[1] - 1], row[col[2] - 1])
+                if i < position[0].nrows:
+                    # print(row[col[0] - 1, col[1] - 1, col[2] - 1])
+                    row = position[0].row_values(i + 1, 0, 9)
+                    if row[0] != '':
+                        return
+                    # print(row[col[0]-1, col[1]-1, col[2]-1])
+                else:
+                    return
 
     def clear_layout(self, layout):
         while layout.count():
